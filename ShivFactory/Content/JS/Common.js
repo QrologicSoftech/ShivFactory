e22e722584d7bde41b48409117b9f170b85aba44
Lokesh Choudhary <Qrologicdeveloper104@gmail.com>
2021-02-22 16:27:32 +0530

    Show loader

--- ShivFactory/Content/JS/Common.js
+++ ShivFactory/Content/JS/Common.js
@@ -1,17 +1,25 @@
 ﻿var common = {
+
     RenderPartialView: function (controller, PartialView) {
-        ajax.doGetAjax(`/${controller}/${controller}/${PartialView}`, function(result) {         
-            $('.partialView').html( result);
+        common.ShowLoader('.partialView');
+        ajax.doGetAjax(`/${controller}/${controller}/${PartialView}`, function (result) {
+            common.HideLoader('.partialView');
+            $('.partialView').html(result);
         })
     },
-
+    ShowLoader: function (idOrClass) {
+        $(idOrClass).addClass('loader-spinner');
+    },
+    HideLoader: function (idOrClass) {
+        $(idOrClass).removeClass('loader-spinner');
+    },
 
     //RenderPartialView: function (contoller, view) {
     //    ajax.doGetAjax(`@Url.Content(/${contoller}/${view})`, `${view}`), function (result) {
     //        $('.partialView').innerHTML = result;
     //    }
     //},
-    
+
 }
 
 
