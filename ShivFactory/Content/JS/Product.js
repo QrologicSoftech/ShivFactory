1d0259092756147e3952bfa30343c44773f31153
Qrololic <Qrologicdeveloper104@gmail.com>
2021-02-25 14:13:16 +0530

    save

--- ShivFactory/Content/JS/Product.js
+++ ShivFactory/Content/JS/Product.js
@@ -1,44 +1,63 @@
 ﻿var product = {
 
     BindSubCategoryByCatId: function (cat_id, subcat_id, mini_id) {
-        var selectedCatId = $(cat_id + " :selected").attr('value');
-        common.ShowLoader(subcat_id);
+       
         var ddlSubCategory = $(subcat_id);
-        var data = { "categoryId": selectedCatId };
-        ajax.doPostAjax(`/Vendor/Vendor/GetSubcategoryByCategoryId`, data, function (result) {
+        var ddlminiCategory = $(mini_id);
+        var selectedCatId = $(cat_id + " :selected").attr('value');
+        debugger;
+        //if (! typeof selectedCatId === "undefined") {
+            common.ShowLoader(subcat_id);
+            var data = { "categoryId": selectedCatId };
             ddlSubCategory.find('option')
                 .remove()
                 .end()
-            if (typeof result !== 'undefined' && result.length > 0) {
-                $.each(result, function (Value, Text) {
-                    ddlSubCategory.append($('<option></option>').val(Text.Value).html(Text.Text));
-                });
-                common.HideLoader(subcat_id);
-            } else {
-                ddlSubCategory.append($('<option></option>').val('-1').html('Selelct'));
-            }
-        });
-    },
-    BindMiniCategoryBySubCatId: function (subcat_id, mini_id) {
-        //alert("Cat Id" + $(subcat_id + " :selected").attr('value'))
-        var selectedSubCatId = $(subcat_id + " :selected").attr('value');
-        common.ShowLoader(mini_id);
-        var ddlMiniCategory = $(mini_id);
-        data = { "subcategoryId": selectedSubCatId }
-        ajax.doPostAjax(`/Vendor/Vendor/GetMinicategoryBySubCategoryId`, data, function (result) {
-            ddlMiniCategory.find('option')
+            ddlSubCategory.append($('<option></option>').val('-1').html('Select'));
+
+            ddlminiCategory.find('option')
                 .remove()
                 .end()
-            if (typeof result !== 'undefined' && result.length > 0) {
-                $.each(result, function (Value, Text) {
-                    ddlMiniCategory.append($('<option></option>').val(Text.Value).html(Text.Text));
-                });
-                common.HideLoader(mini_id);
-            } else {
-                ddlMiniCategory.append($('<option></option>').val('-1').html('Select'));
-            }
-        });
+            ddlminiCategory.append($('<option></option>').val('-1').html('Select'));
+            ajax.doPostAjax(`/Vendor/Vendor/GetSubcategoryByCategoryId`, data, function (result) {
+                if (result.length > 0) {
+                    $.each(result, function (Value, Text) {
+                        ddlSubCategory.append($('<option></option>').val(Text.Value).html(Text.Text));
+                    });
+                    common.HideLoader(subcat_id);
+                }
+            });
+        //} else {
+        //    ddlSubCategory.find('option')
+        //        .remove()
+        //        .end()
+        //    ddlSubCategory.append($('<option></option>').val('-1').html('Select'));
 
+        //    ddlminiCategory.find('option')
+        //        .remove()
+        //        .end()
+        //    ddlminiCategory.append($('<option></option>').val('-1').html('Select'));
+        //}
+    },
+    BindMiniCategoryBySubCatId: function (subcat_id, mini_id) {
+        var selectedSubCatId = $(subcat_id + " :selected").attr('value');
+        if (selectedSubCatId > 0) {
+            common.ShowLoader(mini_id);
+            var ddlMiniCategory = $(mini_id);
+            data = { "subcategoryId": selectedSubCatId }
+            ajax.doPostAjax(`/Vendor/Vendor/GetMinicategoryBySubCategoryId`, data, function (result) {
+                ddlMiniCategory.find('option')
+                    .remove()
+                    .end()
+                if (result.length > 0) {
+                    $.each(result, function (Value, Text) {
+                        ddlMiniCategory.append($('<option></option>').val(Text.Value).html(Text.Text));
+                    });
+                    common.HideLoader(mini_id);
+                } else {
+                    ddlMiniCategory.append($('<option></option>').val('-1').html('Select'));
+                }
+            });
+        }
     }
 
 }
