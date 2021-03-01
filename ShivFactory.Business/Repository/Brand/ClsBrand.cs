076def02caa15d4006def3ba1fbf6950abdc7e58
Lokesh Choudhary <Qrologicdeveloper104@gmail.com>
2021-03-01 15:59:21 +0530

    Changes

---
+++ ShivFactory.Business/Repository/Brand/ClsBrand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ShivFactory.Business.Repository.Brand
+{
+    public class ClsBrand
+    {
+        public int? CategoryId { get; set; }
+        public int? Id { get; set; }
+        [Required(ErrorMessage = "Please enter brand name!!")]
+        public string BrandName { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsActive { get; set; }
+        public HttpPostedFileBase PostedFile { get; set; }
+    }
+}
