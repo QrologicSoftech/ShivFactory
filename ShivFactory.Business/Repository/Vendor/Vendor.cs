6bc76d6654f4b959beb0d1d7f131ebe54896f302
Qrololic <Qrologicdeveloper104@gmail.com>
2021-03-01 13:22:36 +0530

    Vendor Model

---
+++ ShivFactory.Business/Repository/Vendor/Vendor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivFactory.Business.Repository.Vendor
+{
+   public  class Vendor
+    {
+		public int ID { get; set; }
+		
+		public int UserID { get; set; }
+		[Required(ErrorMessage = "Firm Name required")]
+		public string FirmName { get; set; }
+
+		
+		[RegularExpression("^[0-9a-zA-Z \b]+$", ErrorMessage = "Invalid Format")]
+		public string GSTIN { get; set; }
+		public string FullAddress { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+
+		[Required(ErrorMessage ="PAN Number Required"]
+		[RegularExpression("[A-Z]{5}[0-9]{4}[A-Z]{1}",ErrorMessage ="Invalid Format")]
+		public string PanNo { get; set; }
+		public string AddressProofImg { get; set; }
+		public DateTime IsActive { get; set; }
+		public DateTime IsDelete { get; set; }
+		public DateTime AddUpdate { get; set; }
+		public DateTime LastUpdate { get; set; }
+		public string PIN { get; set; }
+		public string Signature { get; set; }
+
+		public string Logo { get; set; }
+	}
+}
---
+++ ShivFactory.Business/Repository/Vendor/VendorBankDetail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivFactory.Business.Repository.Vendor
+{
+    public class VendorBankDetail
+    {
+
+		public int ID { get; set; }
+		public int UserID { get; set; }
+
+		[Required(ErrorMessage="Accounder Holder Name Required")]
+		public string AccountHolderName { get; set; }
+
+		[Required(ErrorMessage = "Must be Valid AccountNumber Required")]
+		[RegularExpression("^\d{9,18}$", ErrorMessage ="Invalid Format")]
+		public string AccountNumber { get; set; }
+
+		[Required(ErrorMessage = "Must be Valid Bank ")]
+		public string BankName { get; set; }
+
+		[Required(ErrorMessage = " IFSC Code  Required")]
+		[RegularExpression("^[A - Z]{4}0[A-Z0-9]{6}$", ErrorMessage = "Invalid Format")]
+		public string IFSCCode { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Branch { get; set; }
+		public DateTime IsActive { get; set; }
+		public DateTime IsDelete { get; set; }
+		public DateTime AddUpdate { get; set; }
+		public DateTime LastUpdate { get; set; }
+	}
+}
