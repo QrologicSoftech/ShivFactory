fd4c0ab2ddb20f70b52b24bd487dc483b5927225
Qrololic <Qrologicdeveloper104@gmail.com>
2021-03-10 15:26:00 +0530

    fix issues

--- ShivFactory/Js/Product.js
+++ ShivFactory/Js/Product.js
@@ -1,29 +1,25 @@
 ﻿var product = {
 
     BindSubCategoryByCatId: function (cat_id, subcat_id, mini_id) {
-
         var ddlSubCategory = $(subcat_id);
         var ddlminiCategory = $(mini_id);
         var selectedCatId = $(cat_id + " :selected").attr('value');
         //if (! typeof selectedCatId === "undefined") {
         common.ShowLoader(subcat_id);
         var data = { "categoryId": selectedCatId };
-        ddlSubCategory.find('option')
-            .remove()
-            .end()
-        ddlSubCategory.append($('<option></option>').val('-1').html('Select'));
-
-        ddlminiCategory.find('option')
-            .remove()
-            .end()
-        ddlminiCategory.append($('<option></option>').val('-1').html('Select'));
+        ddlSubCategory
+            .empty()
+            .append('<option selected="selected" value="-1">Select</option>');
+        ddlminiCategory
+            .empty()
+            .append('<option selected="selected" value="-1">Select</option>');
         ajax.doPostAjax(`/Vendor/Vendor/GetSubcategoryByCategoryId`, data, function (result) {
             if (result.length > 0) {
                 $.each(result, function (Value, Text) {
                     ddlSubCategory.append($('<option></option>').val(Text.Value).html(Text.Text));
                 });
-                common.HideLoader(subcat_id);
             }
+            common.HideLoader(subcat_id);
         });
     },
     BindMiniCategoryBySubCatId: function (subcat_id, mini_id) {
@@ -33,9 +29,8 @@
             var ddlMiniCategory = $(mini_id);
             data = { "subcategoryId": selectedSubCatId }
             ajax.doPostAjax(`/Vendor/Vendor/GetMinicategoryBySubCategoryId`, data, function (result) {
-                ddlMiniCategory.find('option')
-                    .remove()
-                    .end()
+                ddlMiniCategory
+                    .empty()
                 if (result.length > 0) {
                     $.each(result, function (Value, Text) {
                         ddlMiniCategory.append($('<option></option>').val(Text.Value).html(Text.Text));
